0548fd67133e1ae1aa18c54facc58c507ecb94b6
Aliaksandr Hapankou <akkforrs@gmail.com>
2022-11-04 19:08:07 +0200

    feat: add task 03 solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const fs = require('fs/promises');
+const { stat } = require('fs');
+
+let dir = path.join(__dirname, 'secret-folder');
+
+fs.readdir(dir, {withFileTypes: true})
+  .then(files => {
+    for (let file of files) {
+      if (file.isFile()) {
+        let filePath = path.join(dir, file.name);
+        let fileExt = path.extname(filePath);
+        let fileName = path.basename(filePath, fileExt);
+
+        stat(filePath, (error, stats) => {
+          if (error) {
+            console.error(error);
+          } else {
+            let fileSize = stats.size;
+            console.log(fileName + ' - ' + fileExt.slice(1) + ' - ' + fileSize + 'b');
+          }
+        });
+      }
+    }
+  });
